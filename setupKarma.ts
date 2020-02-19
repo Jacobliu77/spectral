90127f2bb08032d9d28ffe8ea37c4a50fa4bc408
nulltoken <emeric.fermas@gmail.com>
2020-02-19 07:51:43 +0100

    WIP

--- setupKarma.ts
+++ setupKarma.ts
@@ -5,6 +5,9 @@ const oasRuleset = JSON.parse(JSON.stringify(require('./rulesets/oas/index.json'
 const oasFunctions = JSON.parse(JSON.stringify(require('./__karma__/__fixtures__/oas-functions.json')));
 const oas2Schema = JSON.parse(JSON.stringify(require('./rulesets/oas/schemas/schema.oas2.json')));
 const oas3Schema = JSON.parse(JSON.stringify(require('./rulesets/oas/schemas/schema.oas3.json')));
+const asyncApiRuleset = JSON.parse(JSON.stringify(require('./rulesets/asyncapi/index.json')));
+const asyncApiFunctions = JSON.parse(JSON.stringify(require('./__karma__/__fixtures__/asyncapi-functions.json')));
+const asyncApi2Schema = JSON.parse(JSON.stringify(require('./rulesets/asyncapi/schemas/schema.asyncapi2.json')));
 
 const { fetch } = window;
 let fetchMock: FetchMockSandbox;
@@ -33,12 +36,27 @@ beforeEach(() => {
     body: JSON.parse(JSON.stringify(oas3Schema)),
   });
 
-  for (const [name, fn] of Object.entries<string>(oasFunctions)) {
-    fetchMock.get(`https://unpkg.com/@stoplight/spectral/rulesets/oas/functions/${name}`, {
-      status: 200,
-      body: fn,
-    });
-  }
+  fetchMock.get('https://unpkg.com/@stoplight/spectral/rulesets/asyncapi/index.json', {
+    status: 200,
+    body: JSON.parse(JSON.stringify(asyncApiRuleset)),
+  });
+
+  fetchMock.get('https://unpkg.com/@stoplight/spectral/rulesets/asyncapi/schemas/schema.asyncapi2.json', {
+    status: 200,
+    body: JSON.parse(JSON.stringify(asyncApi2Schema)),
+  });
+
+  [
+    ['oas', oasFunctions],
+    ['asyncapi', asyncApiFunctions],
+  ].forEach(([rulesetName, funcs]) => {
+    for (const [name, fn] of Object.entries<string>(funcs)) {
+      fetchMock.get(`https://unpkg.com/@stoplight/spectral/rulesets/${rulesetName}/functions/${name}`, {
+        status: 200,
+        body: fn,
+      });
+    }
+  });
 
   fetchMock.get('http://json-schema.org/draft-04/schema', {
     status: 200,
